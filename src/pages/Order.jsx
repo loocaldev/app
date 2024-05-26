9b91597ce55c0fdab99b7a4250b9fab1f097ba33
loocaldev <loocaldigital@gmail.com>
2024-05-26 07:49:28 -0500

    order screen

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,27 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiChevronLeft } from "react-icons/fi";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { useCart } from "../hooks/useCart";
-import ProductCard from "../components/ProductCard"
+import ProductCard from "../components/ProductCard";
 
 import styles from "../styles/Order.module.css";
+
+const formatPriceToCOP = (price) => {
+  const numericPrice = Number(price);
+  if (!isNaN(numericPrice)) {
+    return numericPrice.toLocaleString("es-CO", {
+      style: "currency",
+      currency: "COP",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+  }
+};
 
 function Order() {
   const { cart, addToCart, checkProductInCart, removeFromCart, subtotal } =
     useCart();
   return (
     <>
-      <div className={styles["header"]}>
-        <FiChevronLeft onClick={() => window.history.back()} />
-        <h2>Resumen de tu compra</h2>
+      <div className={styles["order-header"]}>
+        <div className={styles["header-content"]}>
+          <FiChevronLeft onClick={() => window.history.back()} />
+          <h2>Resumen de tu compra</h2>
+        </div>
+        <hr className={styles["divider-header"]} />
       </div>
-      <div>
-      {cart.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+
+      <div className={styles["order-content"]}>
+        {cart.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
-      <div>
-        <p>{subtotal}</p>
+      <div className={styles["order-subtotal"]}>
+        <div className={styles["order-subtotal-content"]}>
+          <p>{formatPriceToCOP(subtotal)}</p>
+          <span>
+            {cart.length} {cart.length === 1 ? "producto" : "productos"}
+          </span>
+        </div>
+        <div className={styles["order-subtotal-button"]}>
+          <p>Confirmar pedido</p>
+          <FiChevronRight />
+        </div>
       </div>
     </>
   );
