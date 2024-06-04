d64415ee6e7d79ca8812f8aa7df1c59d5897e77c
loocaldev <loocaldigital@gmail.com>
2024-06-03 19:40:41 -0500

    receive tx status approved bold

--- src/pages/orderStatus.jsx
+++ src/pages/orderStatus.jsx
@@ -1,43 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 function OrderStatus() {
   const location = useLocation();
-  const [orderId, setOrderId] = useState('');
-  const [transactionStatus, setTransactionStatus] = useState('');
+  const [orderId, setOrderId] = useState("");
+  const [transactionStatus, setTransactionStatus] = useState("");
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
-    const orderIdParam = searchParams.get('bold-order-id');
-    const transactionStatusParam = searchParams.get('bold-tx-status');
-    
+    const orderIdParam = searchParams.get("bold-order-id");
+    const transactionStatusParam = searchParams.get("bold-tx-status");
+
     setOrderId(orderIdParam);
     setTransactionStatus(transactionStatusParam);
 
-    if (orderIdParam && transactionStatusParam === 'approved') {
+    if (orderIdParam && transactionStatusParam === "approved") {
       // Realizar solicitud GET al servidor para obtener la orden correspondiente
-      axios.get(`http://localhost:8000/orders/api/v1/orders/${orderIdParam}`)
-        .then(response => {
+      axios
+        .get(
+          `http://localhost:8000/orders/api/v1/orders/customid/${orderIdParam}`
+        )
+        .then((response) => {
           const orderData = response.data;
+          console.log("Datos de la orden:", orderData); // Imprimir los datos de la orden en la consola
+          console.log(orderData[0].id)
           // Actualizar el estado de la orden en la base de datos si la transacción está aprobada
-          if (orderData.custom_order_id === orderIdParam) {
-            // Realizar solicitud PUT para actualizar el estado de la orden
-            axios.put(`http://localhost:8000/orders/api/v1/orders/${orderIdParam}`, { payment_status: 'approved' })
-              .then(response => {
-                console.log('Estado de la orden actualizado:', response.data);
+          axios
+              .patch(
+                `http://localhost:8000/orders/api/v1/orders/${orderData[0].id}/`,
+                { payment_status: "pending" }
+              )
+              .then((response) => {
+                console.log("Estado de la orden actualizado:", response.data);
               })
-              .catch(error => {
-                console.error('Error al actualizar el estado de la orden:', error);
+              .catch((error) => {
+                console.error(
+                  "Error al actualizar el estado de la orden:",
+                  error
+                );
               });
-          }
         })
-        .catch(error => {
-          console.error('Error al obtener la orden:', error);
+        .catch((error) => {
+          console.error("Error al obtener la orden:", error);
         });
     }
   }, [location.search]);
-
   return (
     <div>
       <h1>Estado de la Orden</h1>
