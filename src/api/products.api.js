8e73a5e216d68c72954f0d89a67fb626bb3b8a6e
loocaldev <loocaldigital@gmail.com>
2024-10-04 18:41:11 -0500

    Update API endopint

--- src/api/products.api.js
+++ src/api/products.api.js
@@ -12,7 +12,7 @@ export const getAllProducts = async () => {
 
 export const createOrder = async () => {
     try {
-        const res = await axios.get('https://44.220.218.144/api/orders/api/v1/orders/');
+        const res = await axios.get('http://44.220.218.144/api/orders/api/v1/orders/');
         return res;
     } catch (error) {
         console.error('Failed to fetch products:', error);
