0cd0a539926a05dac785c7278d9610e618a569d1
loocaldev <loocaldigital@gmail.com>
2024-10-20 11:27:17 -0500

    adjust45242343

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,9 +1,8 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+// Order.jsx
+import React from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import { useCart } from "../hooks/useCart";
 import ProductCard from "../components/ProductCard";
-import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/Order.module.css";
 
@@ -22,62 +21,10 @@ const formatPriceToCOP = (price) => {
 function Order() {
   const { cart, subtotal } = useCart();
   const navigate = useNavigate();
-  const [orderId, setOrderId] = useState(localStorage.getItem("orderId"));
 
-  useEffect(() => {
-    // Generar el ID de la orden si no existe
-    if (!orderId) {
-      const newOrderId = generateOrderId(); // Lógica que ya tienes
-      localStorage.setItem("orderId", newOrderId);
-      setOrderId(newOrderId);
-    }
-  }, []);
-
-
-  const generateOrderId = () => {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = ("0" + (now.getMonth() + 1)).slice(-2);
-    const day = ("0" + now.getDate()).slice(-2);
-    const hour = ("0" + now.getHours()).slice(-2);
-    const minute = ("0" + now.getMinutes()).slice(-2);
-    const second = ("0" + now.getSeconds()).slice(-2);
-    const millisecond = ("00" + now.getMilliseconds()).slice(-3);
-    const productCount = "C" + cart.length;
-    const randomValue = "R" + Math.floor(Math.random() * 9000 + 1000);
-    const orderId = `LC${year}${month}${day}${hour}${minute}${second}${millisecond}${productCount}${randomValue}`;
-    return orderId;
-  };
-
-  const handleConfirmOrder = async () => {
-    try {
-      // Preparar los items de la orden
-      const orderItems = cart.map((item) => ({
-        product_variation_id: item.variationId || null, // Para productos fijos, variationId es null
-        product_id: item.id, // Para productos fijos, usar product_id
-        quantity: item.quantity,
-      }));
-
-      const orderData = {
-        custom_order_id: orderId,
-        items: orderItems, // Enviar todos los productos
-        subtotal: subtotal, // Enviar el subtotal calculado
-      };
-
-      console.log("Datos enviados:", orderData);
-
-      // Crear la orden en el backend
-      const response = await axios.post(
-        "https://loocal.co/api/orders/api/v1/orders/",
-        orderData
-      );
-
-      console.log("Orden creada:", response.data);
-      // Redirigir al checkout
-      navigate("/checkout", { state: { orderId } });
-    } catch (error) {
-      console.error("Error al crear la orden:", error);
-    }
+  const handleConfirmOrder = () => {
+    // Redirigir al checkout con los datos del carrito y subtotal
+    navigate("/checkout", { state: { cart, subtotal } });
   };
 
   return (
